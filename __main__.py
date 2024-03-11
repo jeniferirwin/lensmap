384aa15ceaee04bddab9279183c00610300b084e
Jenifer Irwin <jenifer.irwin@gmail.com>
2024-03-11 11:50:14 -0400

    refactored with less magic

--- __main__.py
+++ __main__.py
@@ -1,23 +1,25 @@
 import os
 import sys
 from world import world
-
-logfile = sys.argv[1]
+from config import *
 
 def main() -> int:
-    if not os.path.isfile(logfile):
-        print("Could not find logfile: {}".format(logfile))
-        sys.exit(1)
+    if not os.path.isdir(LOGDIR):
+        print("Could not find log directory: {}".format(LOGDIR))
+        return(1)
 
-    with open(logfile) as file:
-        log = file.readlines()
-        if not len(log) > 15:
-            print("Not a valid logfile: {}".format(logfile))
+    loglines = []
+    with os.scandir(LOGDIR) as logdir:
+        for entry in logdir:
+            with open(entry) as logfile:
+                input_lines = logfile.readlines()
+                loglines += input_lines
 
-    map = world(1299, 1995)
-    map.log_to_views(log)
+    map = world(WORLD_ROWS, WORLD_COLS)
+    map.log_to_views(loglines)
     map.tidy_views()
     map.printmap()
+    return(0)
 
 if __name__ == '__main__':
     sys.exit(main())
