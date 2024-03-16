a98817330bb8cbcf4528ef7fcd2a41511fb4adfa
Jenifer Irwin <jenifer.irwin@gmail.com>
2024-03-16 12:05:28 -0400

    start using boost library

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,26 +2,38 @@
 #include <fstream>
 #include <stdio.h>
 #include <dirent.h>
-using namespace std;
+#include <string.h>
+#include <boost/filesystem.hpp>
+using namespace boost::filesystem;
 
 const int WORLD_ROWS = 1300;
 const int WORLD_COLS = 2000;
-const char * LOGDIR = "../logfiles/";
+const std::string LOGDIR = "../logfiles/";
+
 char map[WORLD_ROWS][WORLD_COLS] = {};
 
-int get_logfiles() {
-    struct dirent *entry = nullptr;
-    DIR *dp = nullptr;
-    dp = opendir(LOGDIR);
-    if (dp != nullptr) {
-        while ((entry = readdir(dp)))
-            printf("%s\n", entry->d_name);
-    }
-    closedir(dp);
+int get_one_logfile(boost::filesystem::path file) {
+    printf("%s\n", file.filename().c_str());
     return 0;
 }
 
-int fill_with_oceans() {
+int aggregate_logfiles() {
+    path p (LOGDIR);
+    try {
+        if (exists(p)) {
+            for (directory_entry& x : directory_iterator(p))
+                get_one_logfile(x.path());
+        }
+    }
+    
+    catch (const filesystem_error& err) {
+        std::cout << err.what() << '\n';
+    }
+
+    return 0;
+}
+
+void fill_with_oceans() {
     for (int i = 0; i < WORLD_ROWS; i++) {
         for (int j = 0; j < WORLD_COLS; j++) {
             map[i][j] = 'o';
@@ -30,12 +42,7 @@ int fill_with_oceans() {
 }
 
 int main() {
-    ofstream myfile;
-    myfile.open("example.txt");
-    myfile << "Writing this to a file.\n";
-    myfile.close();
-    
-    get_logfiles();
+    aggregate_logfiles();
     fill_with_oceans();
     printf("%c",map[200][3]);
 }
